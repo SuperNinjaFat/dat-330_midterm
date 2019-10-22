1dd93f7c1165383937441158cb566c1d12c3006b
tynanpmatthews <tynanpmatthews@gmail.com>
2019-10-22 14:52:59 -0400

    Testing KNN

--- test.py
+++ test.py
@@ -24,37 +24,49 @@ def loadDatasets():
                 'STAD', 'STES', 'TGCT', 'THCA', 'THYM', 'UCEC', 'UCS', 'UVM']
     patient_info = {}
     #Testing just ACC files, diseases.keys() for full read
-    for disease in diseases[:3]:
+    for disease in diseases[:1]:
         #Collect all clinical data
         filename = 'transposed data/' + disease + '.clin.merged(flipped).csv'
         file = pd.read_csv(filename)
         for i in range(len(file)):
             patient_info[file['patient.bcr_patient_barcode'][i]] = [
-                file['admin.disease_code'][i], file['patient.gender'][i],
-                -file['patient.days_to_birth'][i]]
+                file['admin.disease_code'][i], file['patient.gender'][i]]
+        #-file['patient.days_to_birth'][i]
         #file['patient.stage_event.pathologic_stage'][i] - NaN
         #Attach mutation data to matching clinical data
         for filename in glob.glob('mutations/gdac.broadinstitute.org_' + disease + '.Mutation_Packager_Calls.Level_3.2016012800.0.0/*.maf.txt'):
             file = pd.read_csv(filename, sep='\\t')
             try:
-                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][3] += (len(file))
+                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][2] += (len(file))
             except IndexError:
                 patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()].append(len(file))
-    patient_info = {k:v for k,v in patient_info.items() if len(v) > 3}
+    patient_info = {k:v for k,v in patient_info.items() if len(v) > 2}
     #Transfer patient data to dataframe
     patient_data = pd.DataFrame(data=patient_info)
     patient_data = patient_data.transpose()
-    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2:'Age', 3:'Mutations'})
+    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2:'Mutations'})
 
     for x in range(len(patient_data.columns)):
-        if x not in [2,3]:
+        if x not in [2]:
             patient_data.iloc[:,x] = le.fit_transform(patient_data.iloc[:,x])
 
-    y = patient_data.iloc[:,-1]
+    y = patient_data.iloc[:,-1].astype('int')
     X = patient_data.drop(['Mutations'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     return X_test, y_test, X_train, y_train
+
+def k_neighbors(X_test, y_test, X_train, y_train, k):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_knn = knn.predict(X_test)
+
+    correct = 0
+    for i in range(len(y_knn)):
+        if y_test.iloc[i] == y_knn[i]:
+            correct += 1
+    accuracy = (correct / float(len(y_test))) * 100.0
+    print('KNN Accuracy: ' + repr(round(accuracy, 2)) + '%')
 
 if __name__ == "__main__":
     #If you are... (gender, ethnicity, age, etc...)... you are more/less likely to have
@@ -62,7 +74,7 @@ if __name__ == "__main__":
     ###Percentage for mutation overall, and for specific genes
     start = time.time()
     X_test, y_test, X_train, y_train = loadDatasets()
-    print(X_test, y_test)
+    k_neighbors(X_test, y_test, X_train, y_train, 3)
     print("(Time to complete: " + str(round(time.time() - start, 1)) + "s)")
 
 
