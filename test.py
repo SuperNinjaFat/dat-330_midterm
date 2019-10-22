5988b113e12d669cbabe22683849315a4328f1ea
tynanpmatthews <tynanpmatthews@gmail.com>
2019-10-22 14:39:06 -0400

    Split datasets for training and testing

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.neighbors import KNeighborsClassifier
 
+
 def loadDatasets():
+    le = LabelEncoder()
     warnings.filterwarnings('ignore')
     diseases = ['ACC', 'BLCA', 'BRCA', 'CESC', 'CHOL', 'COAD', 'COADREAD',
                 'DLBC', 'ESCA', 'FPPP', 'GBM', 'GBMLGG', 'HNSC', 'KICH',
@@ -29,37 +31,38 @@ def loadDatasets():
         for i in range(len(file)):
             patient_info[file['patient.bcr_patient_barcode'][i]] = [
                 file['admin.disease_code'][i], file['patient.gender'][i],
-                -file['patient.days_to_birth'][i], file['patient.stage_event.pathologic_stage'][i]]
+                -file['patient.days_to_birth'][i]]
+        #file['patient.stage_event.pathologic_stage'][i] - NaN
         #Attach mutation data to matching clinical data
         for filename in glob.glob('mutations/gdac.broadinstitute.org_' + disease + '.Mutation_Packager_Calls.Level_3.2016012800.0.0/*.maf.txt'):
             file = pd.read_csv(filename, sep='\\t')
             try:
-                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][4] += (len(file))
+                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][3] += (len(file))
             except IndexError:
                 patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()].append(len(file))
-    patient_info = {k:v for k,v in patient_info.items() if len(v) > 4}
+    patient_info = {k:v for k,v in patient_info.items() if len(v) > 3}
     #Transfer patient data to dataframe
     patient_data = pd.DataFrame(data=patient_info)
     patient_data = patient_data.transpose()
-    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2:'Age', 3:'Stage', 4:'Mutations'})
+    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2:'Age', 3:'Mutations'})
 
-    return patient_data
+    for x in range(len(patient_data.columns)):
+        if x not in [2,3]:
+            patient_data.iloc[:,x] = le.fit_transform(patient_data.iloc[:,x])
 
-    #Important data
-    ##print(clinical_data['ACC']['patient.days_to_death']) #float
-    ##print(clinical_data['ACC']['patient.radiation_therapy']) #boolean
-    ##print(clinical_data['ACC']['patient.ethnicity']) #Encode
-    ##print(clinical_data['ACC']['patient.primary_pathology.residual_tumor']) #Encode
-    ##print(clinical_data['ACC']['patient.primary_pathology.lymph_node_examined_count']) #float
-    ##print(clinical_data['ACC']['patient.primary_pathology.histological_type']) #Encode
+    y = patient_data.iloc[:,-1]
+    X = patient_data.drop(['Mutations'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    return X_test, y_test, X_train, y_train
 
 if __name__ == "__main__":
     #If you are... (gender, ethnicity, age, etc...)... you are more/less likely to have
     #more mutations, mutations for... (gene types)... genes.
     ###Percentage for mutation overall, and for specific genes
     start = time.time()
-    patient_data = loadDatasets()
-    print(patient_data)
+    X_test, y_test, X_train, y_train = loadDatasets()
+    print(X_test, y_test)
     print("(Time to complete: " + str(round(time.time() - start, 1)) + "s)")
 
 
