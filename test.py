6349f8a765eb89a4909927418112e5aee7745ec1
tynanpmatthews <tynanpmatthews@gmail.com>
2019-10-22 15:56:30 -0400

    add more clinical data columns

--- test.py
+++ test.py
@@ -25,30 +25,43 @@ def loadDatasets():
                 'STAD', 'STES', 'TGCT', 'THCA', 'THYM', 'UCEC', 'UCS', 'UVM']
     patient_info = {}
     #Testing just ACC files, diseases.keys() for full read
-    for disease in diseases[:1]:
+    for disease in diseases:
         #Collect all clinical data
         filename = 'transposed data/' + disease + '.clin.merged(flipped).csv'
         file = pd.read_csv(filename)
         for i in range(len(file)):
             patient_info[file['patient.bcr_patient_barcode'][i]] = [
-                file['admin.disease_code'][i], file['patient.gender'][i]]
-        #-file['patient.days_to_birth'][i]
-        #file['patient.stage_event.pathologic_stage'][i] - NaN
+                file['admin.disease_code'][i], file['patient.gender'][i],
+                -file['patient.days_to_birth'][i],
+                file['patient.stage_event.pathologic_stage'][i]]
         #Attach mutation data to matching clinical data
         for filename in glob.glob('mutations/gdac.broadinstitute.org_' + disease + '.Mutation_Packager_Calls.Level_3.2016012800.0.0/*.maf.txt'):
             file = pd.read_csv(filename, sep='\\t')
             try:
-                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][2] += (len(file))
+                patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()][4] += (len(file))
             except IndexError:
                 patient_info[filename.split('\\')[-1].split('.')[0][:-3].lower()].append(len(file))
-    patient_info = {k:v for k,v in patient_info.items() if len(v) > 2}
+        print(disease)
+    patient_info = {k:v for k,v in patient_info.items() if len(v) > 4}
     #Transfer patient data to dataframe
     patient_data = pd.DataFrame(data=patient_info)
     patient_data = patient_data.transpose()
-    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2:'Mutations'})
+    patient_data = patient_data.rename(columns={0: 'Disease', 1: 'Gender', 2: 'Age', 3: 'Stage', 4:'Mutations'})
 
+    #Weigh mutation data
+    avg_mut = patient_data['Mutations'].mean()
+    for x in range(len(patient_data['Mutations'])):
+        if patient_data['Mutations'][x] > avg_mut:
+            patient_data['Mutations'][x] = 1
+        else:
+            patient_data['Mutations'][x] = 0
+
+    #Remove any rows with blank data
+    patient_data = patient_data.dropna()
+
+    #Encode non-int columns
     for x in range(len(patient_data.columns)):
-        if x not in [2]:
+        if x not in [2, 4]:
             patient_data.iloc[:,x] = le.fit_transform(patient_data.iloc[:,x])
 
     y = patient_data.iloc[:,-1].astype('int')
@@ -83,17 +96,13 @@ def decision_tree(X_test, y_test, X_train, y_train, k):
 
 
 if __name__ == "__main__":
-    #If you are... (gender, ethnicity, age, etc...)... you are more/less likely to have
-    #more mutations, mutations for... (gene types)... genes.
-    ###Percentage for mutation overall, and for specific genes
     start = time.time()
     X_test, y_test, X_train, y_train = loadDatasets()
+
+    # KNN
     k_neighbors(X_test, y_test, X_train, y_train, 3)
-    print("(Time to complete: " + str(round(time.time() - start, 1)) + "s)")
 
     # Decision trees
-    start = time.time()
-    X_test, y_test, X_train, y_train = loadDatasets()
     decision_tree(X_test, y_test, X_train, y_train, 6)
     print("(Time to complete: " + str(round(time.time() - start, 1)) + "s)")
 
